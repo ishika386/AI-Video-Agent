c9ed9d4ce3734ea2043ad031ed05e67ba81a2161
ishika386 <ishikachoudhary03082006@gmail.com>
2026-07-24 10:10:56 +0530

    Updated files

--- core/transcriber.py
+++ core/transcriber.py
@@ -3,8 +3,7 @@ import os
 import requests
 from pydub import AudioSegment
 
-# Sarvam's sync STT-translate API rejects audio longer than 30s.
-# We slice each chunk into 25s pieces (with a 5s safety margin) before sending.
+
 SARVAM_PIECE_SECONDS = 25
 
 
